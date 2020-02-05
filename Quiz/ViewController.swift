a166540c9b7c2482d652c3f6dfb69718758d2f14
shelly <schou3@unh.newhaven.edu>
2020-02-05 14:21:23 -0500

    I am

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Choudhary, Shelly on 2/5/20.
+//  Copyright © 2020 Choudhary, Shelly. All rights reserved.
+//
+
+import UIKit
+import os
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        os_log("I am in starting phase")
+        // Do any additional setup after loading the view.
+        os_log("rweydfkerhfvmje lbvivcug/oq58vt735vto3t")
+        os_log("abc")
+    }
+
+
+}
+
